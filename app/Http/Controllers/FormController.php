521581b1b7d58f48ea4ba1eb75870dcae9bda913
Aronno Ahsan <aronno.ahsan.sust@gmail.com>
2023-11-22 11:09:50 +0600

    upload working

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,15 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Form;
 
 class FormController extends Controller
 {
-    function form(){
+    function form()
+    {
         return view('form');
     }
-    function store_data(Request $request){
+    function store_data(Request $request)
+    {
         $data = new Form();
         $data->name = $request->input('name');
         $data->gender = $request->input('gender');
+        $data->country = $request->input('country');
+        // Save Checkbox
+        $checkbox_data = $request->input('skill');
+        $data->skill = implode(',', $checkbox_data);
+        // Save Image
+        if ($request->hasFile('profilePicture')) {
+            $image = $request->file('profilePicture');
+            $ext = $image->getClientOriginalExtension();
+            $image_name = time() . "." . $ext;
+            $image->move("storage/images", $image_name);
+            $data->profilePicture = $image_name;
+        }
+        $data->save();
+        return back();
     }
 }
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    function index(){
+    function index()
+    {
         return "hello world";
     }
 }
